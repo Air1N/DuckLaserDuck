62e391b07299242ec5d8fa8bcfb0e81270af9768
Air1N <aarondennisu@gmail.com>
2026-04-12 21:28:53 -0400

    gitignore

--- Scripts/On-hit_Health/DealDamage.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Damage : MonoBehaviour
-{
-    public float damage;
-}
--- Scripts/On-hit_Health/KnockbackForce.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class KnockbackForce : MonoBehaviour
-{
-public float knockbackForce;
-}
